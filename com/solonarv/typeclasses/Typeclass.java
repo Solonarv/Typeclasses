898fc3e93d6770f9e6d9261b5ad5b73bbee84361
Solonarv <nstamm@gmx.de>
2015-12-22 01:06:09 +0100

    Flesh out Typeclass, create Instance

--- com/solonarv/typeclasses/Typeclass.java
+++ com/solonarv/typeclasses/Typeclass.java
@@ -1,5 +1,28 @@
 package com.solonarv.typeclasses;
 
-public class Typeclass {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+import com.solonarv.adts.Type;
+import com.solonarv.adts.Pair;
+
+public abstract class Typeclass {
+	
+	private static final Set<Typeclass> classes = new HashSet<>();
+	
+	public final String name;
+	
+	public Typeclass(String name){
+		this.instances = new HashMap<>();
+		this.name = name;
+		classes.add(this);
+	}
+	
+	private Map<Type[], Instance> instances;
+	
+	public Instance getInstance(Type... types){
+		return instances.get(new Pair<>(this, types));
+	}
 }
